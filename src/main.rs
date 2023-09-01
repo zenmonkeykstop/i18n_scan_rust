d2b5ff45fd9bee87e77f6b4a39b2ee5f54192ae8
Kevin O'Gorman <kog@freedom.press>
2023-09-01 14:29:04 -0400

    arti uses hyper so I guess we'll use it too...

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use clap::{ArgGroup, Parser};
 use serde::Deserialize;
-use reqwest::Error;
 use std::process;
 
-static WEBLATE_ENDPOINT: &str = "https://reblate.securedrop.org/api/projects/securedrop/languages/?format=json";
+use arti_hyper::*;
+use arti_client::{TorClient, TorClientConfig};
+use hyper::body::Buf;
+use hyper::Client;
+use hyper_tls::HttpsConnector;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+static WEBLATE_ENDPOINT: &str = "https://weblate.securedrop.org/api/projects/securedrop/languages/?format=json";
 
 
 #[derive(Deserialize, Debug)]
@@ -20,6 +27,13 @@ struct Language {
     translated_chars: u32,
     translated_chars_percent: f64,
 }
+
+#[derive(Deserialize, Debug)]
+struct Listing {
+    title: String,
+    onion_address: String,
+}
+
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -51,20 +65,26 @@ struct Args {
     workers: u8,
 }
 
-fn get_weblate_data() -> Result<Vec<Language>, Error> {
-    let mut r = reqwest::blocking::get(WEBLATE_ENDPOINT);
-    let languages = r?.json();
-    return languages;
+async fn get_weblate_data() -> Result<Vec<Language>> {
+    let https = HttpsConnector::new();
+    let client = Client::builder()
+        .build::<_, hyper::Body>(https);
+
+    // let client = Client::new();
+    let url = WEBLATE_ENDPOINT.parse().unwrap();
+    let res = client.get(url).await?;
+
+    let body = hyper::body::aggregate(res).await?;
+    let languages = serde_json::from_reader(body.reader())?;
+    Ok(languages)
 }
 
-fn main() {
-    let weblate_data = match get_weblate_data() {
-        Ok(weblate_data) => println!("Found {:?} languages on Weblate!", weblate_data.len()),
-        Err(e) => {
-            eprintln!("Error retrieving Weblate info: {:?}", e);
-            process::exit(1);
-        },
-    };
+// fn get_directory_data() -> Result<Vec<Listing>, Error> {
+
+// }
+
+#[tokio::main]
+async fn main() -> Result<()> {
     let args = Args::parse();
     println!("Use directory? {}!", args.directory);
     if let Some(filename) = args.filename.as_deref() {
@@ -80,4 +100,15 @@ fn main() {
     }
 
     println!("Worker threads: {}", args.workers);
+    /*let weblate_data = match get_weblate_data() {
+        Ok(weblate_data) => println!("Found {:?} languages on Weblate!", weblate_data.len()),
+        Err(e) => {
+            eprintln!("Error retrieving Weblate info: {:?}", e);
+            process::exit(1);
+        },
+    };
+    */
+    let weblate_data = get_weblate_data().await?;
+    println!("Found {:?} languages on Weblate!", weblate_data.len());
+    Ok(())
 }
